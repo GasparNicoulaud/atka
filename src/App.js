2045fdfab3635ffe60d6f238855a2535c4f7bdd9
GasparNicoulaud <g.nicoulaud@gmail.com>
2022-07-22 01:11:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { DepthOfField, EffectComposer } from "@react-three/postprocessing";
-import Model from "./Model";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { useLoader } from "@react-three/fiber";
+
+function Model() {
+  const ref = useRef();
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+
+  const obj = useLoader(OBJLoader, "/modelv1.obj");
+  return <primitive ref={ref} object={obj} />;
+}
 
 export default function Viewer() {
   return (
-    <Canvas shadows dpr={0.2} camera={{ fov: 50 }}>
+    <Canvas dpr={0.5} camera={{ fov: 50 }}>
       <Suspense fallback={null}>
         <Model />
-        <EffectComposer>
-          <DepthOfField focusDistance={1} focalLength={1} bokehScale={1} />
-        </EffectComposer>
       </Suspense>
+      <EffectComposer>
+        <DepthOfField focusDistance={1} focalLength={1} bokehScale={1} />
+      </EffectComposer>
     </Canvas>
   );
 }
